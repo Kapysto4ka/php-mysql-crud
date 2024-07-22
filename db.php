2048bd937eb9a49a085bf8bf3edd6179a416ecff
Anatolii Bilohan <anatolii_bilohan@epam.com>
2024-07-23 01:10:53 +0300

    fixed db.php

--- db.php
+++ db.php
@@ -3,9 +3,9 @@ session_start();
 
 $conn = mysqli_connect(
   getenv('DB_HOST'),
-  getenv('DB_USER'),
-  getenv('DB_PASSWORD'),
-  getenv('DB_NAME')
+  getenv('MYSQL_USER'),
+  getenv('MYSQL_PASSWORD'),
+  getenv('MYSQL_DATABASE')
 ) or die(mysqli_error($conn));
 
 ?>
