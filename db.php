d8f0c3df88f2fdfaee91a844e660701a5daee6ee
Kapysto4ka <115617238+Kapysto4ka@users.noreply.github.com>
2024-07-23 13:34:49 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,15 @@
 <?php
-session_start();
+$servername = "db";
+$username = $_ENV['MYSQL_USER'];
+$password = $_ENV['MYSQL_PASSWORD'];
+$dbname = $_ENV['MYSQL_DATABASE'];
 
-$conn = mysqli_connect(
-  getenv('DB_HOST'),
-  getenv('MYSQL_USER'),
-  getenv('MYSQL_PASSWORD'),
-  getenv('MYSQL_DATABASE')
-) or die(mysqli_error($conn));
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$conn->close();
 ?>
